9c5428f140be3e3bd57e3b61c20b77ba54168669
Abdul Basit <babaralibj362@gmail.com>
2026-01-10 14:26:52 +0500

    code updated

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,7 @@ app.use(
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "http://127.0.0.1:3000",
-  "http://localhost:5000",
-  "http://127.0.0.1:5000",
+  "http://propeople.cloud"
 ];
 
 const corsOptions = {
